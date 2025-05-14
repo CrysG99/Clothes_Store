42502008485f4d37ebcd7017dc71d6fc57ec8d6e
Jonathan Morstad Cats <jonathanmcaats@gmail.com>
2025-05-14 21:52:52 +0200

    Footer and Functions
    Started working on the footer and added a few functions to go with the newly made footer as well

--- static/script.js
+++ static/script.js
@@ -1,3 +1,38 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const overlay = document.getElementById('overlay');
+    const popup = document.getElementById('popup');
+    const closeBtn = document.getElementById('popup-close');
+    const titleBox = document.getElementById('popup-title');
+    const bodyBox = document.getElementById('popup-body');
+
+
+    function openPopup(title, body) {
+        titleBox.textContent = title;
+        bodyBox.textContent = body;
+        overlay.style.display = 'block';
+        popup.style.display = 'block';
+    }
+
+    function closePopup() {
+        overlay.style.display = 'none';
+        popup.style.display = 'none';
+    }
+
+    document.querySelectorAll('.footer-link').forEach(link => {
+        link.addEventListener('click', (e) => {
+            e.preventDefault();
+            const title = link.getAttribute('data-title');
+            const body = link.getAttribute('data-body');
+            openPopup(title, body)
+        });
+    });
+
+    overlay.addEventListener('click', closePopup)
+    closeBtn.addEventListener('click', closePopup)
+
+});
+
+
 
 
 document.addEventListener("DOMContentLoaded", function () {
@@ -8,3 +43,4 @@ document.addEventListener("DOMContentLoaded", function () {
         mobileMenu.classList.toggle("show");
     });
 });
+
